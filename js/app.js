fe29c64b1c5c91ebca43f558ba5ede7c59ad9e50
HipsterBrown <headhipster@hipsterbrown.com>
2022-05-06 19:25:03 -0400

    refactor: convert App into class, use private method on store

--- js/app.js
+++ js/app.js
@@ -1,99 +1,113 @@
-import { addEvent, escapeForHTML, getURLHash } from './helpers.js';
-import { TodoStore } from './store.js';
+import { addEvent, escapeForHTML, getURLHash } from "./helpers.js";
+import { TodoStore } from "./store.js";
 
-(function (window) {
-	'use strict';
+class App {
+	$ = {
+		input: document.querySelector(".new-todo"),
+		list: document.querySelector(".todo-list"),
+		count: document.querySelector(".todo-count strong"),
+		footer: document.querySelector(".footer"),
+		toggleAll: document.querySelector(".toggle-all"),
+		main: document.querySelector(".main"),
+		clear: document.querySelector(".clear-completed"),
+		filters: document.querySelector(".filters"),
+	};
+	filter = getURLHash();
 
-	const Todos = new TodoStore('todo-vanillajs-2022');
-
-	const App = {
-		$: {
-			input:		document.querySelector('.new-todo'),
-			list: 		document.querySelector('.todo-list'),
-			count: 		document.querySelector('.todo-count strong'),
-			footer: 	document.querySelector('.footer'),
-			toggleAll: 	document.querySelector('.toggle-all'),
-			main: 		document.querySelector('.main'),
-			clear: 		document.querySelector('.clear-completed'),
-			filters: 	document.querySelector('.filters'),
-		},
-		filter: getURLHash(),
-		_init: function () {
-			Todos.addEventListener('save', App.render);
-			window.addEventListener('hashchange', () => {
-				App.filter = getURLHash();
-				App.render();
-			});
-			App.$.input.addEventListener('keyup', e => {
-				if (e.key === 'Enter') {
-					App.addTodo(e.target.value);
-					App.$.input.value = '';
-				}
-			});
-			App.$.toggleAll.addEventListener('click', e => {
-				Todos.toggleAll();
-			});
-			App.$.clear.addEventListener('click', e => {
-				Todos.clearCompleted();
-			});
-			App.render();
-		},
-		addTodo: function(todo) {
-			Todos.add({ title: todo, completed: false, id: "id_" + Date.now() });
-		},
-		removeTodo: function(todo) {
-			Todos.remove(todo);
-		},
-		toggleTodo: function(todo) {
-			Todos.toggle(todo);
-		},
-		editingTodo: function(todo, li) {
-			li.classList.add('editing');
-			li.querySelector('.edit').focus();
-		},
-		updateTodo: function(todo, li) {
-			Todos.update(todo);
-			li.querySelector('.edit').value = todo.title;
-		},
-		createTodoItem: function(todo) {
-			const li = document.createElement('li');
-			if (todo.completed) { li.classList.add('completed'); }
-	
-			li.innerHTML = `
-				<div class="view">
-					<input class="toggle" type="checkbox" ${todo.completed ? 'checked' : ''}>
-					<label>${escapeForHTML(todo.title)}</label>
-					<button class="destroy"></button>
-				</div>
-				<input class="edit" value="${escapeForHTML(todo.title)}">`;
-
-			addEvent(li, '.destroy', 'click', () => App.removeTodo(todo, li));
-			addEvent(li, '.toggle', 'click', () => App.toggleTodo(todo, li));
-			addEvent(li, 'label', 'dblclick', () => App.editingTodo(todo, li));
-			addEvent(li, '.edit', 'keyup', e => {
-				if (e.key === 'Enter') App.updateTodo({ ...todo, title: e.target.value }, li)
-			});
-			addEvent(li, '.edit', 'blur', e => App.updateTodo({ ...todo, title: e.target.value }, li));
-
-			return li;
-		},
-		render: function() {
-			const todosCount = Todos.all().length;
-			App.$.filters.querySelectorAll('a').forEach(el => el.classList.remove('selected'));
-			App.$.filters.querySelector(`[href="#/${App.filter}"]`).classList.add('selected');
-			App.$.list.innerHTML = '';
-			Todos.all(App.filter).forEach(todo => {
-				App.$.list.appendChild( App.createTodoItem(todo) );
-			});
-			App.$.footer.style.display = todosCount ? 'block' : 'none';
-			App.$.main.style.display = todosCount ? 'block' : 'none';
-			App.$.clear.style.display = Todos.hasCompleted() ? 'block' : 'none';
-			App.$.toggleAll.checked = todosCount && Todos.isAllCompleted();
-			App.$.count.innerHTML = Todos.all('active').length;
-		}
+	constructor() {
+		this.store = new TodoStore("todo-vanillajs-2022");
+		this.store.addEventListener("save", this.render.bind(this));
+		window.addEventListener("hashchange", () => {
+			this.filter = getURLHash();
+			this.render();
+		});
+		this.$.input.addEventListener("keyup", (event) => {
+			if (event.key === "Enter") {
+				this.addTodo(event.target.value);
+				this.$.input.value = "";
+			}
+		});
+		this.$.toggleAll.addEventListener("click", () => {
+			this.store.toggleAll();
+		});
+		this.$.clear.addEventListener("click", () => {
+			this.store.clearCompleted();
+		});
+		this.render();
 	}
 
-	App._init();
+	addTodo(todo) {
+		this.store.add({ title: todo, completed: false, id: `id_${Date.now()}` });
+	}
 
-	window.TodoApp = App;
-})(window);
+	removeTodo(todo) {
+		this.store.remove(todo);
+	}
+
+	toggleTodo(todo) {
+		this.store.toggle(todo);
+	}
+
+	editingTodo(_todo, li) {
+		li.classList.add("editing");
+		li.querySelector(".edit").focus();
+	}
+
+	updateTodo(todo, li) {
+		this.store.update(todo);
+		li.querySelector(".edit").value = todo.title;
+	}
+
+	createTodoItem(todo) {
+		const li = document.createElement("li");
+		if (todo.completed) {
+			li.classList.add("completed");
+		}
+
+		li.innerHTML = `
+      <div class="view">
+        <input class="toggle" type="checkbox" ${
+					todo.completed ? "checked" : ""
+				}>
+        <label>${escapeForHTML(todo.title)}</label>
+        <button class="destroy" aria-label="remove todo"></button>
+      </div>
+      <input class="edit" value="${escapeForHTML(todo.title)}">
+    `;
+
+		addEvent(li, ".destroy", "click", () => this.removeTodo(todo, li));
+		addEvent(li, ".toggle", "click", () => this.toggleTodo(todo, li));
+		addEvent(li, "label", "dblclick", () => this.editingTodo(todo, li));
+		addEvent(li, ".edit", "keyup", (event) => {
+			if (event.key === "Enter")
+				this.updateTodo({ ...todo, title: event.target.value }, li);
+		});
+		addEvent(li, ".edit", "blur", (event) =>
+			this.updateTodo({ ...todo, title: event.target.value }, li)
+		);
+
+		return li;
+	}
+
+	render() {
+		const todosCount = this.store.all().length;
+		this.$.filters
+			.querySelectorAll("a")
+			.forEach((el) => el.classList.remove("selected"));
+		this.$.filters
+			.querySelector(`[href="#/${this.filter}"]`)
+			.classList.add("selected");
+		this.$.list.innerHTML = "";
+		this.store.all(this.filter).forEach((todo) => {
+			this.$.list.appendChild(this.createTodoItem(todo));
+		});
+		this.$.footer.style.display = todosCount ? "block" : "none";
+		this.$.main.style.display = todosCount ? "block" : "none";
+		this.$.clear.style.display = this.store.hasCompleted() ? "block" : "none";
+		this.$.toggleAll.checked = todosCount && this.store.isAllCompleted();
+		this.$.count.innerHTML = this.store.all("active").length;
+	}
+}
+
+new App();
+
--- readme.md
+++ readme.md
@@ -13,6 +13,13 @@ Related poll: "Would you build a large web app in 2022 with Vanilla JS?" https:/
 
 Critism, PRs and feedback welcome!
 
+## Local Development
+
+```
+npm install
+npm start
+```
+
 ## License
 
 <a rel="license" href="http://creativecommons.org/licenses/by/4.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by/4.0/80x15.png" /></a><br />This <span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/InteractiveResource" rel="dct:type">work</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://sindresorhus.com" property="cc:attributionName" rel="cc:attributionURL">TasteJS</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/deed.en_US">Creative Commons Attribution 4.0 International License</a>.
